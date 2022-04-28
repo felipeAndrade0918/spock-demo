5469c80e62b3e2d65ea4fe6346c8e08dd529f175
Felipe Andrade <felipe.andrade0918@gmail.com>
2022-04-27 21:52:53 -0300

    Adiciona configurações para rodar sem uma chave da API

--- src/main/java/com/batutinhas/spock/demo/external/client/GiantBombClient.java
+++ src/main/java/com/batutinhas/spock/demo/external/client/GiantBombClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "giantBomApi", url = "https://www.giantbomb.com/api")
+@FeignClient(value = "giantBomApi", url = "${api.host}")
 public interface GiantBombClient {
 
     @GetMapping(value = "/search?api_key=${api.key}&format=json&resources=game",
