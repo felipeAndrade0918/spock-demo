75c3ebcac365168b660cd36366c68df0add539c1
Felipe Andrade <felipe.andrade0918@gmail.com>
2023-03-02 13:25:35 -0300

    Remove ordenação

--- src/main/java/com/batutinhas/spock/demo/domain/Game.java
+++ src/main/java/com/batutinhas/spock/demo/domain/Game.java
@@ -9,15 +9,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Builder
-public class Game implements Comparable<Game> {
+public class Game {
 
     private String name;
     private String description;
     private String coverImage;
     private String originalReleaseDate;
 
-    @Override
-    public int compareTo(Game otherGame) {
-        return this.getName().compareTo(otherGame.getName());
-    }
 }
