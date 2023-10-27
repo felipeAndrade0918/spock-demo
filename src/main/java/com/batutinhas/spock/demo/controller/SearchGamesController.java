a5b6a3a9d2a7a9d6949a4575da9d7a8c2909956c
Felipe Andrade <felipe.andrade0918@gmail.com>
2023-10-26 21:52:25 -0300

    Remove imports desnecessários

--- src/main/java/com/batutinhas/spock/demo/controller/SearchGamesController.java
+++ src/main/java/com/batutinhas/spock/demo/controller/SearchGamesController.java
@@ -1,14 +1,11 @@
 package com.batutinhas.spock.demo.controller;
 
-import com.batutinhas.spock.demo.domain.Game;
 import com.batutinhas.spock.demo.domain.GameSearch;
 import com.batutinhas.spock.demo.usecase.SearchGamesUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
